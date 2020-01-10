09e92b20a1eb494a73bc564f486e545d8ff2b3a8
illyakaynov <ikaynov@gmail.com>
2020-01-11 00:54:39 +0100

    added vnc_driver to setup

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ pip install lxml
 pip install tensorflow-gpu==1.14.0
 pip install universe
 pip install beautifulsoup4
+pip install go_vncdriver
 export PATH=$PATH:/home/vbti/user_data/local/go/bin
 
