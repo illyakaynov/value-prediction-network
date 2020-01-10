a33c77e981e33f0670ce507137bbad122a4277c4
Illya Kaynov <ikaynov@gmail.com>
2020-01-10 21:14:04 +0000

    added script to install dependencies

---
+++ setup.sh
@@ -0,0 +1,12 @@
+pip install numpy
+pip install gym==0.7.0
+pip install gym[atari]
+pip install scipy
+pip install pyaml
+pip install opencv-python 
+pip install lxml
+pip install tensorflow-gpu==1.14.0
+pip install universe
+pip install beautifulsoup4
+export PATH=$PATH:/home/vbti/user_data/local/go/bin
+
